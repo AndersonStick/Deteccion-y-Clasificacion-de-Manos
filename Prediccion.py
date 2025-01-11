73bf4747c65157f7c3d8718e059ea768dbfd99ce
Anderson Barrera <andersonbar10@gmail.com>
2025-01-11 16:10:17 -0500

    Cambio rutas Prediccion

--- Prediccion.py
+++ Prediccion.py
@@ -5,66 +5,59 @@ import numpy as np
 from keras_preprocessing.image import load_img, img_to_array
 from keras.models import load_model
 
+# Rutas de los archivos
+modelo = r'D:\SEMESTRE7\Deteccion-y-Clasificacion-de-Manos\ModeloVocales.keras'
+peso = r'D:\SEMESTRE7\Deteccion-y-Clasificacion-de-Manos\pesosVocales.weights.h5'
 
-r'D:\SEMESTRE7\Dedos\Modelo.h5'
+# Cargamos el modelo y los pesos
+cnn = load_model(modelo)  # Cargamos el modelo (incluye los pesos integrados)
+cnn.load_weights(peso)  # Cargamos los pesos si están en un archivo separado
 
-modelo = r'D:\SEMESTRE7\Dedos\Modelo.h5'
-peso =  r'D:\SEMESTRE7\Dedos\pesos.h5'
-cnn = load_model(modelo)  #Cargamos el modelo
-cnn.load_weights(peso)  #Cargamos los pesos
-
-direccion = 'C:/Users/santi/Desktop/Universidad/9 Semestre/Vision Python/Manos/Fotos/Validacion'
+# Ruta de las imágenes de validación
+direccion = r'D:\SEMESTRE7\Dedos\Fotos\Validacion'
 dire_img = os.listdir(direccion)
 print("Nombres: ", dire_img)
 
-#Leemos la camara
+# Leemos la cámara
 cap = cv2.VideoCapture(0)
 
-#----------------------------Creamos un obejto que va almacenar la deteccion y el seguimiento de las manos------------
-clase_manos  =  mp.solutions.hands
-manos = clase_manos.Hands() #Primer parametro, FALSE para que no haga la deteccion 24/7
-                            #Solo hara deteccion cuando hay una confianza alta
-                            #Segundo parametro: numero maximo de manos
-                            #Tercer parametro: confianza minima de deteccion
-                            #Cuarto parametro: confianza minima de seguimiento
+# Creamos un objeto para almacenar la detección y el seguimiento de las manos
+clase_manos = mp.solutions.hands
+manos = clase_manos.Hands()  # Parámetros predeterminados para la detección de manos
 
-#----------------------------------Metodo para dibujar las manos---------------------------
-dibujo = mp.solutions.drawing_utils #Con este metodo dibujamos 21 puntos criticos de la mano
+# Método para dibujar las manos
+dibujo = mp.solutions.drawing_utils  # Dibujamos los 21 puntos clave de la mano
 
-
-while (1):
-    ret,frame = cap.read()
+while True:
+    ret, frame = cap.read()
     color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     copia = frame.copy()
     resultado = manos.process(color)
-    posiciones = []  # En esta lista vamos a almcenar las coordenadas de los puntos
-    #print(resultado.multi_hand_landmarks) #Si queremos ver si existe la deteccion
+    posiciones = []  # Coordenadas de los puntos clave de la mano
 
-    if resultado.multi_hand_landmarks: #Si hay algo en los resultados entramos al if
-        for mano in resultado.multi_hand_landmarks:  #Buscamos la mano dentro de la lista de manos que nos da el descriptor
-            for id, lm in enumerate(mano.landmark):  #Vamos a obtener la informacion de cada mano encontrada por el ID
-                alto, ancho, c = frame.shape  #Extraemos el ancho y el alto de los fotpgramas para multiplicarlos por la proporcion
-                corx, cory = int(lm.x*ancho), int(lm.y*alto) #Extraemos la ubicacion de cada punto que pertence a la mano en coordenadas
-                posiciones.append([id,corx,cory])
+    if resultado.multi_hand_landmarks:  # Si se detectan manos
+        for mano in resultado.multi_hand_landmarks:
+            for id, lm in enumerate(mano.landmark):
+                alto, ancho, c = frame.shape
+                corx, cory = int(lm.x * ancho), int(lm.y * alto)
+                posiciones.append([id, corx, cory])
                 dibujo.draw_landmarks(frame, mano, clase_manos.HAND_CONNECTIONS)
-            if len(posiciones) != 0:
-                pto_i1 = posiciones[3] #5 Dedos: 4 | 0 Dedos: 3 | 1 Dedo: 2 | 2 Dedos: 3 | 3 Dedos: 4 | 4 Dedos: 8
-                pto_i2 = posiciones[17]#5 Dedos: 20| 0 Dedos: 17| 1 Dedo: 17| 2 Dedos: 20| 3 Dedos: 20| 4 Dedos: 20
-                pto_i3 = posiciones[10]#5 Dedos: 12| 0 Dedos: 10 | 1 Dedo: 20|2 Dedos: 16| 3 Dedos: 12| 4 Dedos: 12
-                pto_i4 = posiciones[0] #5 Dedos: 0 | 0 Dedos: 0 | 1 Dedo: 0 | 2 Dedos: 0 | 3 Dedos: 0 | 4 Dedos: 0
+
+            if posiciones:
                 pto_i5 = posiciones[9]
-                x1,y1 = (pto_i5[1]-80),(pto_i5[2]-80) #Obtenemos el punto incial y las longitudes
-                ancho, alto = (x1+80),(y1+80)
-                x2,y2 = x1 + ancho, y1 + alto
+                x1, y1 = (pto_i5[1] - 80), (pto_i5[2] - 80)
+                ancho, alto = (x1 + 80), (y1 + 80)
+                x2, y2 = x1 + ancho, y1 + alto
                 dedos_reg = copia[y1:y2, x1:x2]
-                dedos_reg = cv2.resize(dedos_reg, (200, 200), interpolation=cv2.INTER_CUBIC)  # Redimensionamos las fotos
-                x = img_to_array(dedos_reg)  # Convertimos la imagen a una matriz
-                x = np.expand_dims(x, axis=0)  # Agregamos nuevo eje
-                vector = cnn.predict(x)  # Va a ser un arreglo de 2 dimensiones, donde va a poner 1 en la clase que crea correcta
-                resultado = vector[0]  # [1,0] | [0, 1]
-                respuesta = np.argmax(resultado)  # Nos entrega el indice del valor mas alto 0 | 1
+                dedos_reg = cv2.resize(dedos_reg, (200, 200), interpolation=cv2.INTER_CUBIC)
+                x = img_to_array(dedos_reg)
+                x = np.expand_dims(x, axis=0)
+                vector = cnn.predict(x)
+                resultado = vector[0]
+                respuesta = np.argmax(resultado)
+
                 if respuesta == 1:
-                    print(vector,resultado)
+                    print(vector, resultado)
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
                     cv2.putText(frame, '{}'.format(dire_img[0]), (x1, y1 - 5), 1, 1.3, (0, 255, 0), 1, cv2.LINE_AA)
                 elif respuesta == 0:
@@ -72,20 +65,10 @@ while (1):
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
                     cv2.putText(frame, '{}'.format(dire_img[1]), (x1, y1 - 5), 1, 1.3, (0, 0, 255), 1, cv2.LINE_AA)
 
-    cv2.imshow("Video",frame)
+    cv2.imshow("Video", frame)
     k = cv2.waitKey(1)
-    if k == 27:
+    if k == 27:  # Presiona 'Esc' para salir
         break
+
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
